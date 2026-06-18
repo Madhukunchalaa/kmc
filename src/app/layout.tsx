a8a7fe9dfc1105f6f179e514d5d04e0ed98602be
Madhu Kunchala <madhkunchala@gmail.com>
2026-06-18 07:31:27 +0530

    chnages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,9 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body className={`${cinzel.variable} ${cinzelDecorative.variable} ${cormorant.variable} ${raleway.variable}`} suppressHydrationWarning>
-        <Shell>{children}</Shell>
+        <div id="page-wrapper">
+          <Shell>{children}</Shell>
+        </div>
       </body>
     </html>
   );
